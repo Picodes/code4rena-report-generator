2361c8fa83ea307d41c4a7d5ce5ed5f3582f4877
EV_om <0xEV_om@proton.me>
2024-05-28 18:33:16 +0200

    Update FoTTokens.ts

--- src/issues/M/FoTTokens.ts
+++ src/issues/M/FoTTokens.ts
@@ -6,7 +6,7 @@ import util from 'util';
 const issue: ASTIssue = {
   regexOrAST: 'AST',
   type: IssueTypes.M,
-  title: 'Contracts are vulnerable to fee-on-transfer accounting-related issues',
+  title: 'Contracts are vulnerable to fee-on-transfer and rebasing accounting-related issues',
   description:
     "Consistently check account balance before and after transfers for Fee-On-Transfer discrepancies. As arbitrary ERC20 tokens can be used, the amount here should be calculated every time to take into consideration a possible fee-on-transfer or deflation.\nAlso, it's a good practice for the future of the solution.\n\nUse the balance before and after the transfer to calculate the received amount instead of assuming that it would be equal to the amount passed as a parameter. Or explicitly document that such tokens shouldn't be used and won't be supported",
   detector: (files: InputType): Instance[] => {
